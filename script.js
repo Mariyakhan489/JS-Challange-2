7929bf86197d56ba332a35cc238b2662c422dc10
Mariyakhan489 <maryakhan489@gmail.com>
2026-03-31 11:24:00 -0700

    JS Challenge-2

---
+++ script.js
@@ -0,0 +1,18 @@
+// Coding Challenge # 2
+
+let markWeight = 78;
+let markHeight = 1.69;
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+//  BMI
+let markBmi = markWeight / (markHeight * markHeight);
+let johnBmi = johnWeight / (johnHeight * johnHeight);
+
+if (markBmi > johnBmi) {
+    console.log(`Mark's BMI ${markBmi} is Haigher than John's BMI ${johnBmi}`);
+}
+else{
+    console.log(`John's BMI ${bmiMark} is Haigher than Mark's BMI ${bmiJohn}`);
+
+}
